c0a0d0aa365230ef495d66c815a26d5a1652692a
MatteoEURECOM <68018653+MatteoEURECOM@users.noreply.github.com>
2020-12-11 21:21:49 +0100

    Add files via upload

---
+++ code/setup.sh
@@ -0,0 +1,7 @@
+pip install numpy==1.17.2
+pip install open3d-python==0.7.0.0
+pip install pandas==1.0.3 
+pip install h5py==2.10.0
+pip install tensorflow-gpu==1.14.0
+pip install keras==2.3.1
+pip install matplotlib==3.1.1
